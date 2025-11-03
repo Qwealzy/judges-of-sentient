a8de3d071b13b5f6f579d0c0cb2d263b162f33d4
Qwealzy <yalinakagac1@gmail.com>
2025-11-03 19:32:48 +0300

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,7 @@
-// src/types.ts
-import type { Database } from './lib/database.types';
-
-export type { Database };
-
-export type PledgeResult =
-  Database['public']['Tables']['pledges']['Row'];
-
-export type PledgeInsert =
-  Database['public']['Tables']['pledges']['Insert'];
-
-export type PledgeUpdate =
-  Database['public']['Tables']['pledges']['Update'];
+export type PledgeResult = {
+  id: string;
+  username: string;
+  profile_image_url: string;
+  description: string;
+  created_at: string;
+};
